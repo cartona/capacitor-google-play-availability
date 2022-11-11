bf04273cedc643b3841a26149ff5d2edb7287409
ramikhafagi96 <ramikh.developer@gmail.com>
2022-11-12 00:00:38 +0200

    Docs: add documentation for methods

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,12 +1,27 @@
 export interface GooglePlayServicesPlugin {
+  /**
+   * Retrieve google play services availability status
+   */
   checkGooglePlayServicesAvailability(): Promise<{ enabled: boolean }>;
+  /**
+   * Retrieve enabling google play services request result
+   */
   requestEnableGooglePlayServices(): Promise<{ success: boolean }>;
+  /**
+   * Display a dialog with a message before enabling google play services request and retrieve the result
+   */
   requestEnableGooglePlayServicesWithMessage(
-    requestAlert: RequestAlert,
+    requestDialog: RequestDialog,
   ): Promise<{ success: boolean }>;
 }
 
-export interface RequestAlert {
+export interface RequestDialog {
+  /**
+   * Message displayed in the dialog before displaying requesting enabling google play services
+   */
   message: string;
+  /**
+   * Label of the button in the dialog
+   */
   btnLabel: string;
 }
